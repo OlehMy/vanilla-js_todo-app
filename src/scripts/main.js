7acc37f2245911f5373c9ed45a717874a2a5d3fe
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-12-07 06:10:22 +0200

    add, remove, update status and count, toggle all implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,73 @@
 'use strict';
+
+const root = document.querySelector('.todoapp');
+const newTodoField = root.querySelector('.new-todo');
+const itemsList = root.querySelector('.todo-list');
+const allToggler = root.querySelector('.toggle-all');
+
+const updateInfo = () => {
+  const notCompletedTogglers = root.querySelectorAll('.toggle:not(:checked)');
+  const counter = root.querySelector('.todo-count');
+
+  counter.innerHTML = `${notCompletedTogglers.length} items left`;
+  allToggler.checked = notCompletedTogglers.length === 0;
+};
+
+allToggler.addEventListener('change', () => {
+  const togglers = root.querySelectorAll('.toggle');
+  const checked = allToggler.checked;
+
+  for (const toggler of togglers) {
+    toggler.checked = checked;
+    toggler.closest('.todo-item').classList.toggle('completed', checked);
+  }
+});
+
+newTodoField.addEventListener('keydown', (event) => {
+  if (event.key !== 'Enter') {
+    return;
+  };
+
+  if (!newTodoField.value) {
+    return;
+  };
+
+  const id = +new Date();
+
+  itemsList.insertAdjacentHTML('beforeend', `
+    <li class="todo-item">
+      <input
+        type="checkbox"
+        class="toggle"
+        id="toggle-${id}"
+      >
+      <label for="toggle-${id}">${newTodoField.value}</label>
+      <button type="button" class="destroy"></button>
+    </li>
+  `);
+
+  newTodoField.value = '';
+
+  updateInfo();
+});
+
+itemsList.addEventListener('click', (event) => {
+  if (!event.target.matches('.destroy')) {
+    return;
+  }
+
+  event.target.closest('.todo-item').remove();
+  updateInfo();
+});
+
+itemsList.addEventListener('change', (event) => {
+  if (!event.target.matches('.toggle')) {
+    return;
+  }
+
+  event.target
+    .closest('.todo-item')
+    .classList.toggle('completed', event.target.checked);
+
+  updateInfo();
+});
